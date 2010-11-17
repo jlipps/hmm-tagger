56333ccfb5fa7d9758e06bd0b0f7ae0565ab6011
Jonathan Lipps <jlipps@gmail.com>
2010-11-17 09:08:12 +0000

    added penntags class, removed misc.py

---
+++ PennTags.py
@@ -0,0 +1,24 @@
+######## PennTags.py ########
+
+class PennTags:
+    "A class to hold information about tags in the Penn Treebank"
+
+    proper_noun = 'NNP'
+    common_noun = 'NN'
+    pl_proper_noun = 'NNPS'
+    pl_common_noun = 'NNS'
+    verb_pp = 'VBN'
+    verb = 'VB'
+    det = 'DT'
+    cardinal = 'CD'
+    adv = 'RB'
+    gerund = 'VBG'
+    adj = 'JJ'
+    prep = 'IN'
+    comp_adv = 'RBR'
+    comp_adj = 'JJR'
+    verb_3s = 'VBZ'
+    unknown = 'UNK'
+    default = 'NN'
+    default_upper = 'NNP'
+    rare_tags = ['--','UNK'] # tags to add to POS list before testing
