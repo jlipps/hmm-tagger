6de1ee6ccef03ded8de7fd50632df32ca3e36d85
Jonathan Lipps <jlipps@gmail.com>
2012-02-22 16:59:39 -0800

    removed clean.py since it was redundant.

--- clean.py
+++
@@ -1,8 +0,0 @@
-######### clean.py #########
-
-from TreebankCleaner import TreebankCleaner # import cleaning class
-
-# initialize treebank cleaner with the appropriate path and file(s)
-t = TreebankCleaner('/Users/jlipps/Code/hmm-tagger/', ['treebank3_sect2.txt'])
-
-t.clean()
