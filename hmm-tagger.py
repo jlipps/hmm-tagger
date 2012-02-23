29e7996b035b0e803f5c20235eca179838e5a014
Jonathan Lipps <jlipps@gmail.com>
2012-02-22 16:42:49 -0800

    added --clean option to hmm-tagger.py, added README

--- hmm-tagger.py
+++ hmm-tagger.py
@@ -3,12 +3,13 @@
 from TreebankCleaner import TreebankCleaner # import cleaning class
 from Tagger import Tagger # import the tagging controller
 import os # for path info
+import sys # for command line options
 
-# initialize treebank cleaner with the current path and pre-downloaded file(s)
-t = TreebankCleaner(os.getcwd()+'/', ['treebank3_sect2.txt'])
-
-# do cleaning
-t.clean()
+if '--clean' in sys.argv:
+  # initialize treebank cleaner with the current path and pre-downloaded file(s)
+  t = TreebankCleaner(os.getcwd()+'/', ['treebank3_sect2.txt'])
+  # do cleaning
+  t.clean()
 
 # initialize a tagging object with the cleaned corpus file(s)
 t = Tagger(os.getcwd()+'/', ['treebank3_sect2.txt_cleaned'])
